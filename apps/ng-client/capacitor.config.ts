a0d235e58adda2cf73acaaa5a993cd7c576509e6
Marlon Harrison <marlon@ionic.io>
2022-11-01 21:46:24 -0500

    add cap

---
+++ apps/ng-client/capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'NGClientApp',
+  webDir: '../../dist/apps/ng-client',
+  bundledWebRuntime: false
+};
+
+export default config;
